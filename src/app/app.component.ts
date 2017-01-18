8931e9cb277871163cc267dd0e23f84b123fa8a0
geek <trungnd@evolableasia.vn>
2017-01-18 17:21:05 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{ (item | async)?.name }}</h1>
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+  item: FirebaseObjectObservable<any>;
+  constructor(af: AngularFire) {
+    this.item = af.database.object('/contents');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWTOP-ADRJ0gInos8xPj_C8h5tGcsYtGI",
+  authDomain: "sticker-store-8846d.firebaseapp.com",
+  databaseURL: "https://sticker-store-8846d.firebaseio.com",
+  storageBucket: "sticker-store-8846d.appspot.com",
+  messagingSenderId: "831121864728"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+imports: [
+BrowserModule,
+AngularFireModule.initializeApp(firebaseConfig)
+],
+declarations: [ AppComponent ],
+bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
