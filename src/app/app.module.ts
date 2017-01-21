35b6c2c2028467c88765c138a01e3d7b3f4d9ad1
geek <trung1706@gmail.com>
2017-01-21 14:30:36 +0700

    Add firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { StickerDetailComponent } from './sticker-detail.component';
 import { StickersComponent } from './stickers.component';
 import { StickerService } from './sticker.service';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWTOP-ADRJ0gInos8xPj_C8h5tGcsYtGI",
+  authDomain: "sticker-store-8846d.firebaseapp.com",
+  databaseURL: "https://sticker-store-8846d.firebaseio.com",
+  storageBucket: "sticker-store-8846d.appspot.com",
+  messagingSenderId: "831121864728"
+};
 
 @NgModule({
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     AppRoutingModule
   ],
